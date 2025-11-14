f37ceb8021fea163e5a4f4b5ba5bb56b71fcdae9
Your Name <your.email@example.com>
2025-11-14 02:13:14 +0000

    added link file and descriptions to the code base

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,16 @@
-// Create a new router
+// Import Express module and create a router
 const express = require("express")
 const router = express.Router()
 
-// Handle our routes
+// Display the home page
 router.get('/',function(req, res, next){
     res.render('index.ejs')
 });
 
+// Display the about page
 router.get('/about',function(req, res, next){
     res.render('about.ejs')
 });
 
-// Export the router object so index.js can access it
+// Export the router so index.js can use it
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,21 @@
-// Create a new router
+// Import Express module
 const express = require("express")
+
+// Create a router to handle routes separately
 const router = express.Router()
 
+// Display the registration form page
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
+// Process form submission and display confirmation message
 router.post('/registered', function (req, res, next) {
     // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last +
+             ' you are now registered! We will send an email to you at ' + 
+             req.body.email);                                                                              
 }); 
 
-// Export the router object so index.js can access it
+// Export the router so index.js can use it
 module.exports = router
